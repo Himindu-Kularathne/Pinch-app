bb5597410d4904782f755e896f96ac33b1f57335
Himindu <himindukularathne@gmail.com>
2023-12-19 15:49:26 +0530

    All magnification gestures and ui components are designed

---
+++ Pinch/View/ControlImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Himindu Kularathne on 2023-12-19.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControlImageView()
+}
---
+++ Pinch/View/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Himindu Kularathne on 2023-12-19.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
